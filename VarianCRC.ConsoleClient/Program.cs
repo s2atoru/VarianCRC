675d654661002363fad185378bd4108a7b70ec94
Carlos Anderson <carlosjanderson@gmail.com>
2015-07-10 15:55:55 -0400

    Add CRC console client

---
+++ VarianCRC.ConsoleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace VarianCRC.ConsoleClient
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: crc <file_path>");
+                return 1;
+            }
+
+            string filePath = args[0];
+            string fileContents = null;
+
+            try
+            {
+                using (StreamReader reader = File.OpenText(filePath))
+                {
+                    fileContents = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was a problem reading the file.");
+                return 1;
+            }
+
+            Console.WriteLine("{0:X4}", CRC.CalculateCRC(fileContents));
+
+            return 0;
+        }
+    }
+}
